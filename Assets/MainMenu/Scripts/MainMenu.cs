738573bcb881e734b3fd7b39851aed3e219f167a
luyuhao <luyuhao00@gmail.com>
2017-02-20 13:32:05 +0800

    Add mainMenu

---
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    void OnGUI()
+    {
+
+        if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
+            Debug.Log("Clicked the button with text");
+
+    }
+}
