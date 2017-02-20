5973e67363d790df9dbbb3bd9bf3502cf18902e5
LUYUHAO <LUYU0004@E.NTU.EDU.SG>
2017-02-20 17:12:12 +0800

    my own change

--- Assets/MainMenu/Scripts/MainMenu.cs
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -7,8 +7,9 @@ public class MainMenu : MonoBehaviour {
     void OnGUI()
     {
 
+        
         if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
-            Debug.Log("Clicked the button with text");
+            Debug.Log("Change from my own laptop!");
 
     }
 }
